9909d1a7fcd8d7e0b04ad2ad3dd488ffc2a41a7b
Micha White <botahamec@outlook.com>
2022-07-02 18:48:49 -0400

    Implemented the lint with tests

--- clippy_lints/src/unused_box.rs
+++ clippy_lints/src/unused_box.rs
@@ -1,5 +1,10 @@
-use rustc_lint::LateLintPass;
+use clippy_utils::{diagnostics::span_lint_and_sugg, ty::implements_trait};
+use rustc_errors::Applicability;
+use rustc_hir::{intravisit::FnKind, Body, FnDecl, FnRetTy, HirId};
+use rustc_lint::{LateContext, LateLintPass};
 use rustc_session::{declare_lint_pass, declare_tool_lint};
+use rustc_span::Span;
+use rustc_typeck::hir_ty_to_ty;
 
 declare_clippy_lint! {
     /// ### What it does
@@ -8,7 +13,9 @@ declare_clippy_lint! {
     ///
     /// ### Why is this bad?
     ///
-    /// It's better to just return `T` in these cases
+    /// It's better to just return `T` in these cases. The caller may not need
+    /// the value to be boxed, and it's expensive to free the memory once the
+    /// `Box<T>` been dropped.
     ///
     /// ### Example
     /// ```rust
@@ -25,8 +32,50 @@ declare_clippy_lint! {
     #[clippy::version = "1.64.0"]
     pub UNUSED_BOX,
     nursery,
-    "default lint description"
+    "Needlessly returning a Box"
 }
 declare_lint_pass!(UnusedBox => [UNUSED_BOX]);
 
-impl LateLintPass<'_> for UnusedBox {}
+impl LateLintPass<'_> for UnusedBox {
+    fn check_fn(
+        &mut self,
+        cx: &LateContext<'_>,
+        fn_kind: FnKind<'_>,
+        decl: &FnDecl<'_>,
+        _: &Body<'_>,
+        _: Span,
+        _: HirId,
+    ) {
+        // it's unclear what part of a closure you would span, so for now it's ignored
+        // if this is changed, please also make sure not to call `hir_ty_to_ty` below
+        if matches!(fn_kind, FnKind::Closure) {
+            return;
+        }
+
+        let FnRetTy::Return(return_ty_hir) = &decl.output else { return };
+
+        // this is safe, since we're not in a body
+        let return_ty = hir_ty_to_ty(cx.tcx, return_ty_hir);
+
+        if !return_ty.is_box() {
+            return;
+        }
+
+        let boxed_ty = return_ty.boxed_ty();
+        let Some(sized_trait) = cx.tcx.lang_items().sized_trait() else { return };
+
+        // it's sometimes useful to return Box<T> if T is unsized, so don't lint those
+        if implements_trait(cx, boxed_ty, sized_trait, &[]) {
+            span_lint_and_sugg(
+                cx,
+                UNUSED_BOX,
+                return_ty_hir.span,
+                format!("function returns `Box<{0}>` when `{0}` implements `Sized`", boxed_ty).as_str(),
+                "change the return type to",
+                boxed_ty.to_string(),
+                // the return value also needs to be changed, so this can't be MachineApplicable
+                Applicability::HasPlaceholders,
+            );
+        }
+    }
+}
--- tests/ui/unused_box.rs
+++ tests/ui/unused_box.rs
@@ -1,5 +1,31 @@
 #![warn(clippy::unused_box)]
 
+struct Foo {}
+
+// lint
+fn boxed_usize() -> Box<usize> {
+    Box::new(5)
+}
+
+// lint
+fn boxed_string() -> Box<Foo> {
+    Box::new(Foo {})
+}
+
+// don't lint: str is unsized
+fn boxed_str() -> Box<str> {
+    "Hello, world!".to_string().into_boxed_str()
+}
+
+// don't lint: this has an unspecified return type
+fn default() {}
+
+// don't lint: this doesn't return a Box
+fn string() -> String {
+    String::from("Hello, world")
+}
+
 fn main() {
-    // test code goes here
+    // don't lint: this is a closure
+    let a = || -> Box<usize> { Box::new(5) };
 }
