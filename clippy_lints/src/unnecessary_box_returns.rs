59d2100df0b39f86193f36dfd77e2b7c419f9296
dswij <dswijj@gmail.com>
2023-01-08 02:22:13 +0800

    fix: updated imports

--- clippy_lints/src/unnecessary_box_returns.rs
+++ clippy_lints/src/unnecessary_box_returns.rs
@@ -1,10 +1,10 @@
 use clippy_utils::{diagnostics::span_lint_and_sugg, ty::implements_trait};
 use rustc_errors::Applicability;
 use rustc_hir::{intravisit::FnKind, Body, FnDecl, FnRetTy, HirId};
+use rustc_hir_analysis::hir_ty_to_ty;
 use rustc_lint::{LateContext, LateLintPass};
 use rustc_session::{declare_lint_pass, declare_tool_lint};
 use rustc_span::Span;
-use rustc_typeck::hir_ty_to_ty;
 
 declare_clippy_lint! {
     /// ### What it does
