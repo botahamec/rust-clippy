20ad382d7b5a171757c1300b02effe3a9bdaba28
dswij <dswijj@gmail.com>
2023-01-08 01:42:42 +0800

    Update `unnecessary_box_returns` declaration

--- clippy_lints/src/lib.rs
+++ clippy_lints/src/lib.rs
@@ -941,6 +941,7 @@ pub fn register_plugins(store: &mut rustc_lint::LintStore, sess: &Session, conf:
     store.register_late_pass(|_| Box::new(allow_attributes::AllowAttribute));
     store.register_late_pass(move |_| Box::new(manual_main_separator_str::ManualMainSeparatorStr::new(msrv())));
     store.register_late_pass(|_| Box::new(unnecessary_struct_initialization::UnnecessaryStruct));
+    store.register_late_pass(|_| Box::new(unnecessary_box_returns::UnnecessaryBoxReturns));
     // add lints here, do not remove this comment, it's used in `new_lint`
 }
 
